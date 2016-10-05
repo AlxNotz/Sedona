b560be804c18c1e964ca258cb5b5348ce37198f9
alxnotz <ionweber@mail.ru>
2016-10-05 21:04:07 +0300

    search-form add/subtract done

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,12 @@ var nav = document.querySelector('.nav');
 var navList = document.querySelector('.nav__list');
 var menu = document.querySelector('.nav__menu');
 var cross = document.querySelector('.nav__close');
+var hotelSearchBtn = document.querySelector('.hotel-search__header');
+var hotelSearchBody = document.querySelector('.hotel-search__body');
+var formMinus = document.querySelector('.hotel-search__decrease');
+var formPlus = document.querySelector('.hotel-search__increase');
+var adultCount = document.getElementById('adult-count');
+var childrenCount = document.getElementById('children-count');
 
 var menuClosed = true;
 
@@ -20,7 +26,7 @@ window.onresize = function() {
 	}
 }
 
-menu.addEventListener('click', function(event) {
+menu.addEventListener('click', function() {
 	if (menuClosed) {
 		document.body.insertBefore(navList, document.body.firstElementChild);
 		navList.style.display = 'flex';
@@ -31,3 +37,36 @@ menu.addEventListener('click', function(event) {
 
 	menuClosed = !menuClosed;
 });
+
+hotelSearchBtn.onmousedown = function() {
+	return false;
+}
+
+hotelSearchBtn.addEventListener('click', function() {
+	hotelSearchBody.classList.toggle('hotel-search__body--js-closed');
+});
+
+document.querySelector('.hotel-search__wrap').onclick = function(event) {
+	var target = event.target;
+
+	if (target.classList.contains('hotel-search__decrease')) {
+		event.preventDefault();
+		changeCount(target, '-');
+	}
+
+	if (target.classList.contains('hotel-search__increase')) {
+		event.preventDefault();
+		changeCount(target, '+');
+	}
+}
+
+function changeCount(target, operator) {
+	var input = operator == '-' ? target.nextElementSibling
+															: target.previousElementSibling;
+	
+	if (operator == '-' && input.value != 0) {
+		input.value--;
+	} else if (operator == '+') {
+		input.value++;
+	}
+}
